68a3ffed14af699db284d83319b510aa8a0e8f89
zhanyeye <zhanyeye@qq.com>
2019-06-13 23:42:34 +0800

    event_listening

---
+++ myexperiment01_event_listening/src/main/java/com/example/myexperiment01_event_listening/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myexperiment01_event_listening;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.act_main_editText_input);
+        final Button button = findViewById(R.id.act_main_button_submit);
+        final TextView textView = findViewById(R.id.act_main_textView_view);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() == 0) {
+                    button.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String string = editText.getText().toString();
+                textView.setText(string);
+            }
+        });
+
+    }
+
+
+
+
+}
